8fc1f8e1ff544b975472e6ee753cba40f7f79614
RUDRA PRASAD GORAI <rpgprasadacc99@gmail.com>
2026-07-23 10:22:40 +0530

    Refactor password generator with error handling

--- main.py
+++ main.py
@@ -1,26 +1,51 @@
-import random
-
 def password_generator():
-    n = int(input("Number of chars (maximum 30) : "))
-    if n > 40:
-        return ("max. chars ---> 30 \n Password can't generate....😑😑😐")
+    import random
+    import time
+    class NumberError(Exception):
+        pass
+    try:
+        no_char = int(input("📌 Enter the number of charcter (must >4) : "))
+        print("\n📌 Processing.....\n")
+        time.sleep(1)
+        upper_char = "QWERTYUYIOPASDFGHJKLZXCVBNM" 
+        lower_char = "QWERTYUYIOPASDFGHJKLZXCVBNM".lower()
+        symbl = "!@#$%&?/"
+        number = "123456789"
+        password = ""
+        if no_char>=4:
+            password +=  random.choice(upper_char)+random.choice(lower_char)+random.choice(symbl)+random.choice(number)
+        else:
+            raise NumberError("Value must be greater than 4 💀💀💀❌❌❌")
+        all_char = upper_char+lower_char+symbl+number
+        if len(all_char)<no_char:
+            raise OverflowError("Character overflow 💀💀❌")
+        while len(password)<no_char:
+            new_char = random.choice(all_char)
+            if new_char not in password:
+                password += new_char
+        # print(password)
+        final_password = ' '.join(random.sample(password,len(password)))
+    except ValueError:
+        print("📌 Processing.......")
+        time.sleep(1)
+        print("Wrong Input enter ❌❌")
+    except NumberError as e:
+        # print("📌 Processing.......")
+        # time.sleep(1)
+        print(e)
+    except OverflowError as e:
+        print(e)
+        
     else:
-        lower = "abcdefghijklmnopqrstuvwxyz"
-        upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        digits = "0123456789"
-        symbols = "!@#$%&?|"
-        password = ''
-        password += random.choice(lower)
-        password += random.choice(upper)
-        password += random.choice(digits)
-        password += random.choice(symbols)
-        all_chars = lower + upper + digits + symbols
-        while len(password) < n :
-            char = random.choice(all_chars)
-            if char not in password:
-                password += char
-        password = ''.join(random.sample(password , len(password)))
-        return password
+        print("✅ Password generating.......\n")
+        time.sleep(1)
+        uni_char = 1
+        temp_char = ""
+        print("📌 Checking Password (All Unique Charcters) : ")
+        time.sleep(2)
+        print(final_password)
 
-print(password_generator())
-
+        
+    finally:
+        print("Thank You ✨✨✨")
+password_generator()
